c2670f34da25597ae6b7b53ce396007d7c3529f8
jaeyoungjang2 <lion85488@gmail.com>
2022-10-10 00:27:05 +0900

    kakao login 추가

--- src/main/java/com/sparta/personalStudy/repository/UserRepository.java
+++ src/main/java/com/sparta/personalStudy/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.personalStudy.repository;
 
+
 import com.sparta.personalStudy.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByKakaoId(Long kakaoId);
+    Optional<User> findByEmail(String email);
 }
